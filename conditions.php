1c5edbbaf674fc407a92d84cf106ad94c3c8a9dc
zerobank <zeedlaesadbn@gmail.com>
2017-01-09 13:33:26 +0700

    Add files via upload

---
+++ conditions.php
@@ -0,0 +1,77 @@
+<?php 
+session_start();
+if ($_SESSION){ 
+		if ($_SESSION['email']=="admin@admin.com")
+		{include "include/header3.php";}
+	else {include "include/header2.php";}
+	}
+	else  
+	{include "include/header.php";}
+	include "include/conn.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title>N-Air a E-commerce category Flat Bootstrap Responsive Website Template | Contact :: w3layouts</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="keywords" content="N-Air Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+		<script type="application/x-javascript"> addEventListener("load", function() {setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		<meta charset utf="8">
+		<!--fonts-->
+		<link href='//fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
+		
+		<!--fonts-->
+		<!--bootstrap-->
+			 <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+		<!--coustom css-->
+			<link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <!--shop-kart-js-->
+        <script src="js/simpleCart.min.js"></script>
+		<!--default-js-->
+			<script src="js/jquery-2.1.4.min.js"></script>
+		<!--bootstrap-js-->
+			<script src="js/bootstrap.min.js"></script>
+		<!--script-->
+         <!-- FlexSlider -->
+            <script src="js/imagezoom.js"></script>
+              <script defer src="js/jquery.flexslider.js"></script>
+            <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+
+            <script>
+            // Can also be used with $(document).ready()
+            $(window).load(function() {
+              $('.flexslider').flexslider({
+                animation: "slide",
+                controlNav: "thumbnails"
+              });
+            });
+            </script>
+        <!-- //FlexSlider-->
+    </head>
+    <body>
+       
+        <div class="head-bread">
+            <div class="container">
+                <ol class="breadcrumb">
+                    <li><a href="index.php">Home</a></li>
+                    <li class="active">Contact</li>
+                </ol>
+            </div>
+        </div>
+        <!-- contact -->
+        <div class="contact">
+            <div class="container">
+                <h3>เงือนไขการซื้อขายสินค้า</h3>
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3826.4307862437863!2d99.51363175082032!3d16.453710588588713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30de18f6ca6700f1%3A0xabb985de7aa854c5!2z4Lih4Lir4Liy4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4Lij4Liy4LiK4Lig4Lix4LiP4LiB4LmN4Liy4LmB4Lie4LiH4LmA4Lie4LiK4Lij!5e0!3m2!1sth!2sth!4v1478891678242" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </h3>
+                <div class="contact-content">
+                    
+                    <div class="map"></div>
+                </div>
+            </div>
+        </div>
+	   <!--contact-->
+        <?php include "include/footer.php";?>
+    </body>
+</html>
---
+++ tfootball.php
@@ -0,0 +1,105 @@
+<?php 
+session_start();
+if ($_SESSION){ 
+		if ($_SESSION['email']=="admin@admin.com")
+		{include "include/header3.php";}
+	else {include "include/header2.php";}
+	}
+	else  
+	{include "include/header.php";}
+	include "include/conn.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title>N-Air a E-commerce category Flat Bootstrap Responsive Website Template | Products :: w3layouts</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="keywords" content="N-Air Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+		<script type="application/x-javascript"> addEventListener("load", function() {setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		<meta charset utf="8">
+		<!--fonts-->
+		<link href='//fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
+		
+		<!--fonts-->
+        <!--form-css-->
+        <link href="css/form.css" rel="stylesheet" type="text/css" media="all" />
+		<!--bootstrap-->
+			 <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+		<!--coustom css-->
+			<link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <!--shop-kart-js-->
+        <script src="js/simpleCart.min.js"></script>
+		<!--default-js-->
+			<script src="js/jquery-2.1.4.min.js"></script>
+		<!--bootstrap-js-->
+			<script src="js/bootstrap.min.js"></script>
+		<!--script-->
+         <!-- FlexSlider -->
+            <script src="js/imagezoom.js"></script>
+              <script defer src="js/jquery.flexslider.js"></script>
+            <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+
+            <script>
+            // Can also be used with $(document).ready()
+            $(window).load(function() {
+              $('.flexslider').flexslider({
+                animation: "slide",
+                controlNav: "thumbnails"
+              });
+            });
+            </script>
+        <!-- //FlexSlider-->
+    </head>
+    <body>
+        
+                                            
+        <div class="head-bread">
+            <div class="container">
+                <ol class="breadcrumb">
+                    <li><a href="index.php">Home</a></li>
+                    <li class="active">PRODUCTS</li>
+                </ol>
+            </div>
+        </div>
+        <div class="products-gallery">
+           <div class="container">
+               <div class="col-md-9 grid-gallery">
+                    <?php
+$sql = "SELECT * FROM product WHERE ptid=00001;" ;	
+$result = $db->query($sql);
+while($row = $result->fetch_array()){
+	$product_id=$row['product_id'];
+    $pname=$row['pname'];
+    $price=$row['price'];
+    $ppid=$row['ppid'];
+	
+?>
+                    <div class="col-md-4 grid-stn simpleCart_shelfItem">
+                         <!-- normal -->
+                            <div class="ih-item square effect3 bottom_to_top">
+                                <div class="bottom-2-top">
+                        <div class="img"><img src="upload/<?php echo $ppid; ?>" alt="/" class="img-responsive gri-wid"></div>
+                                <div class="info">
+                                    <div class="pull-left styl-hdn">
+                                        <h3><?php echo $pname; ?></h3>
+                                    </div>
+                                    <div class="pull-right styl-price">
+                                        <p><a  href="#" class="item_add"><span class="glyphicon glyphicon-shopping-cart grid-cart" aria-hidden="true"></span> <span class=" item_price"><?php echo $price; ?></span></a></p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div></div>
+                            </div>
+                        <!-- end normal -->
+                        <div class="quick-view">
+                           <a href="single.php?id=<?php echo $row['product_id'];?>"> Quick view </a>
+                        </div></div>
+                        <?php }?>
+                        <?php include "include/menu.php";?>
+                    </div>
+           
+            </div> 
+        </div>
+        <?php include "include/footer.php";?>
+    </body>
+</html>
